a4fd344e7349504cd532892d1b34ed5bb09bfdde
HollowMax <maxholovatyi5@gmail.com>
2023-04-17 20:30:00 +0200

    Add correct basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="test-tweets">
+    <BrowserRouter basename="/test-tweets">
       <App />
     </BrowserRouter>
   </React.StrictMode>
